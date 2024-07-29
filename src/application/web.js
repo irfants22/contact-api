fc59bb02934f65e1438d79a528a4ee7ed280deed
irfants22 <irfanee02@gmail.com>
2024-07-29 10:53:27 +0700

    add cors

--- src/application/web.js
+++ src/application/web.js
@@ -2,8 +2,10 @@ import express from "express";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../route/public-api.js";
 import { authorizedRouter } from "../route/api.js";
+import cors from "cors";
 
 export const web = express();
+web.use(cors());
 web.use(express.json());
 
 web.use(publicRouter);
